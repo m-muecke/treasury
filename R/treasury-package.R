98dcb6d30172526f4cab98a900c24aa86cc731a1
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-01-24 11:26:35 +0100

    fix: missing imports

--- R/treasury-package.R
+++ R/treasury-package.R
@@ -2,10 +2,13 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom httr2 iterate_with_offset
 #' @importFrom httr2 req_perform
+#' @importFrom httr2 req_perform_iterative
 #' @importFrom httr2 req_url_query
 #' @importFrom httr2 req_user_agent
 #' @importFrom httr2 request
 #' @importFrom httr2 resp_body_xml
+#' @importFrom httr2 resps_data
 ## usethis namespace: end
 NULL
