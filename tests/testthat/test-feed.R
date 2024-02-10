d656508f76765f5cd8b7255ae251aba29232f673
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-02-10 14:59:05 +0100

    tests: tests for clean_yield_curve and clean_bill_rates

--- tests/testthat/test-feed.R
+++ tests/testthat/test-feed.R
@@ -17,3 +17,59 @@ test_that("input validation works", {
     expect_error(fn(1L))
   }
 })
+
+test_that("clean_yield_curve works", {
+  data <- data.frame(
+    date = rep("2020-02-03", 13L),
+    maturity = c(
+      "BC_1MONTH", "BC_2MONTH", "BC_3MONTH", "BC_6MONTH", "BC_1YEAR",
+      "BC_2YEAR", "BC_3YEAR", "BC_5YEAR", "BC_7YEAR", "BC_10YEAR",
+      "BC_20YEAR", "BC_30YEAR", "BC_30YEARDISPLAY"
+    ),
+    rate = rnorm(13L)
+  )
+  actual <- clean_yield_curve(data)
+  expected <- data[1:12, ]
+  expected$maturity <- c(
+    "1 month", "2 month", "3 month", "6 month",
+    "1 year", "2 year", "3 year", "5 year",
+    "7 year", "10 year", "20 year", "30 year"
+  )
+  expect_equal(actual, expected)
+})
+
+test_that("clean_bill_rates works", {
+  date <- rep("2020-02-03", 10L)
+  rate <- 1:10
+  type <- c(
+    "ROUND_B1_CLOSE_4WK_2",
+    "ROUND_B1_YIELD_4WK_2",
+    "ROUND_B1_CLOSE_8WK_2",
+    "ROUND_B1_YIELD_8WK_2",
+    "ROUND_B1_CLOSE_13WK_2",
+    "ROUND_B1_YIELD_13WK_2",
+    "ROUND_B1_CLOSE_26WK_2",
+    "ROUND_B1_YIELD_26WK_2",
+    "ROUND_B1_CLOSE_52WK_2",
+    "ROUND_B1_YIELD_52WK_2"
+  )
+  data <- data.frame(date = date, type = type, value = rate)
+  actual <- clean_bill_rates(data)
+  type <- rep(c("close", "yield"), 5L)
+  maturity <- c(
+    "4 weeks",
+    "4 weeks",
+    "8 weeks",
+    "8 weeks",
+    "13 weeks",
+    "13 weeks",
+    "26 weeks",
+    "26 weeks",
+    "52 weeks",
+    "52 weeks"
+  )
+  expected <- data.frame(
+    date = date, type = type, maturity = maturity, value = rate
+  )
+  expect_equal(actual, expected)
+})
