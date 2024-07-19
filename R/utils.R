29306d860d50f36b2fa2f0ed16afeb1e2c74d348
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-07-19 11:43:40 +0200

    refactor: remove redundant isTRUE

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,6 @@
-is_installed <- function(pkg) {
-  isTRUE(requireNamespace(pkg, quietly = TRUE))
-}
-
 as_tibble <- function(x) {
-  if (getOption("treasury.use_tibble", TRUE) && is_installed("tibble")) {
+  if (getOption("treasury.use_tibble", TRUE) &&
+        requireNamespace("tibble", quietly = TRUE)) {
     tibble::as_tibble(x)
   } else {
     x
